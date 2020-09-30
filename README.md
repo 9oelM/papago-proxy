09d9e324f565a3aa7c66d594134e463db6ae6556
9oelM <hj923@hotmail.com>
2020-10-01 02:21:14 +0900

    ADD: docs and more options

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # papago-proxy
+
+This is a very simple implementation of papago proxy server.
+
+## Usage
+
+GET `/translate?text={text}&wait_for_msec={msec}`
+
+querystrings: 
+- `text`: required. text to be translated
+- `wait_for_msec`: optional. custom option to wait on headless Chrome until translate is fully done. This can be set if the text is too long to be translated in a short time. This is to be removed later.
+
+## Example
+
+Request:
+```
+GET localhost:3000/translate?text=hello
+```
+
+Response:
+```json
+{
+  "text": "안녕하십니까"
+}
+```
--- src/crawler/crawler.ts
+++ src/crawler/crawler.ts
@@ -1,6 +1,7 @@
+import { ElementHandle } from 'puppeteer';
 import { Cluster } from 'puppeteer-cluster';
 import { Consts } from './constants';
-import { IncomingText, TaskFunctionArguments, TranslatedText, TranslationCrawlerCluster } from './types';
+import { ButtonElement, IncomingText, InputElement, TaskFunctionArguments, TranslatedText, TranslationCrawlerCluster, TranslationOptions } from './types';
 import { waitForNetworkIdle } from './util';
 
 export const launchCrawlerCluster: () => Promise<TranslationCrawlerCluster> = async () => 
@@ -13,15 +14,21 @@ export const launchCrawlerCluster: () => Promise<TranslationCrawlerCluster> = as
     maxConcurrency: Consts.CONFIG.MAX_CONCURRENCY,
   });
 
-export const runTranslation = async (cluster: TranslationCrawlerCluster, incomingText: IncomingText) => {
+export const runTranslation = async (cluster: TranslationCrawlerCluster, incomingText: IncomingText, options?: TranslationOptions) => {
   await cluster.task(async ({ page, data: incomingText }: TaskFunctionArguments<IncomingText>) => {
     await page.goto(Consts.URLs.PAPAGO, { waitUntil : ['load', 'domcontentloaded']});
     await page.waitForSelector(Consts.IDs.INCOMING_TEXT_INPUT_ID);
-    await page.type(Consts.IDs.INCOMING_TEXT_INPUT_ID, incomingText);
-    await waitForNetworkIdle(page, 500, 0);
+    await page.$eval(Consts.IDs.INCOMING_TEXT_INPUT_ID, (el: InputElement, incomingTextInsideEval) => {
+      el.value = incomingTextInsideEval
+    }, incomingText);
+    await page.type(Consts.IDs.INCOMING_TEXT_INPUT_ID, Consts.ENTER);
+    await waitForNetworkIdle(
+      page, 
+      options?.waitForMSecs !== undefined ? options.waitForMSecs : Consts.DEFAULT_WAIT_MSECS, 
+      0,
+    );
     const translatedText: TranslatedText | null = await page.$eval(Consts.IDs.TRANSLATED_TEXT_ID, el => el.textContent);
-
-    return translatedText === null ? Consts.ERROR_TEXT_NOT_FOUND : translatedText;
+    return translatedText === null || translatedText.trim() === '' ? Consts.ERROR_TEXT_NOT_FOUND : translatedText;
   });
 
   const translatedText = await cluster.execute(incomingText);
